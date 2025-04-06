b2b49d14e2b519474edfe7050f37bfff931527f7
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-06 19:32:34 -0300

    feat: Configura injeção de dependencia.

--- backend/services/TestInbox.Api/Program.cs
+++ backend/services/TestInbox.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TestInbox.Api.Application.Interfaces;
+using TestInbox.Api.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=emails.db"));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
