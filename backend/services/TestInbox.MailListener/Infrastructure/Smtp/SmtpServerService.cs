4a59cab0fbc7210a106c8f67adc012a8a759dabb
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-06 16:32:25 -0300

    feat: Adiciona o SmtpServerService

---
+++ backend/services/TestInbox.MailListener/Infrastructure/Smtp/SmtpServerService.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Hosting;
+using SmtpServer;
+
+namespace TestInbox.MailListener.Infrastructure.Smtp;
+
+public class SmtpServerService : IHostedService
+{
+    private readonly SmtpServer.SmtpServer _smtpServer;
+
+    public SmtpServerService(ISmtpServerOptions options, IServiceProvider serviceProvider)
+    {
+        _smtpServer = new SmtpServer.SmtpServer(options, serviceProvider);
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _smtpServer.StartAsync(cancellationToken);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _smtpServer?.Shutdown();
+        return Task.CompletedTask;
+    }
+}
