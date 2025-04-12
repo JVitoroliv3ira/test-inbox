e86de11605759e01c79deb7d69061d075d2af3de
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-11 22:15:05 -0300

    feat: Adiciona header da aplicação

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header className="w-full border-b bg-background text-foreground">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">TestInbox</h1>
+      </div>
+    </header>
+    );
+}
