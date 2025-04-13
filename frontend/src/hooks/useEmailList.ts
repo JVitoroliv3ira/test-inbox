87af886f72f6f85cfb7d7027da471172226a3035
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-12 22:07:44 -0300

    feat: Adiciona hook responsável por gerenciar a busca dos e-mails

---
+++ frontend/src/hooks/useEmailList.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { fetchEmails } from "@/lib/api";
+import { EmailListItem } from "@/types/output/email";
+import { PaginatedResult } from "@/types/output/paginated";
+
+export function useEmailList(pageSize = 10) {
+  const [result, setResult] = useState<PaginatedResult<EmailListItem>>();
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [debouncedSearch, setDebouncedSearch] = useState(searchTerm);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedSearch(searchTerm);
+      setPage(0);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchData(page, debouncedSearch);
+  }, [page, debouncedSearch]);
+
+  const fetchData = async (page: number, term: string) => {
+    setLoading(true);
+    try {
+      const res = await fetchEmails({ page, pageSize, searchTerm: term });
+      setResult(res);
+    } catch (err) {
+      console.error("Erro ao buscar e-mails", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    result,
+    loading,
+    page,
+    setPage,
+    searchTerm,
+    setSearchTerm,
+  };
+}
