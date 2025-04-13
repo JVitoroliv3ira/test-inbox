aea5fae903bc34bb3a2298bb1467a13d05852462
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-12 22:09:06 -0300

    feat: Utiliza listagemd de e-mails

--- frontend/src/features/email/pages/EmailListPage.tsx
+++ frontend/src/features/email/pages/EmailListPage.tsx
@@ -1,9 +1,15 @@
 import { Layout } from "@/components/layout";
+import { EmailList } from "../components/emailList";
 
 export function EmailListPage() {
   return (
     <Layout>
-      <h2 className="text-xl font-semibold mb-4">📥 Lista de E-mails</h2>
+      <div className="w-full h-[calc(100vh-170px)] grid grid-cols-1 md:grid-cols-5">
+        <div className="col-span-1 md:col-span-3 p-3 overflow-y-auto">
+          <EmailList />
+        </div>
+        <div className="col-span-1 md:col-span-2 p-3 bg-blue-900"></div>
+      </div>
     </Layout>
   );
 }
